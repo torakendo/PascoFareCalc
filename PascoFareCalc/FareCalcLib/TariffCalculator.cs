2340d0a771f8a2111482d276e34032d034a81398
YuriAkema <yuri.akema@sun-asterisk.com>
2020-05-25 16:58:35 +0900

    GetPrice()の加算あり、両端加算処理実装

--- PascoFareCalc/FareCalcLib/TariffCalculator.cs
+++ PascoFareCalc/FareCalcLib/TariffCalculator.cs
@@ -94,16 +94,60 @@ namespace FareCalcLib
                     (r.horizontal_step_from == horizontalValue && r.horizontal_step_to == horizontalValue))
                 );
 
+
             if (tariffDetailQuery.Count() == 1)
             {
+                var td = tariffDetailQuery.Select(tdr => tdr).ToArray()[0];
+
                 // set the tariff price before adding
-                var base_charge_before_adding = tariffDetailQuery.Select(tdr => tdr.tariff_price).ToArray()[0];
+                var base_charge_before_adding = td.tariff_price;
+                decimal vertical_adding_price = 0;        // 縦軸の加算額
+                decimal horizontal_adding_price = 0;      // 横軸の加算額
+                decimal vertical_adding_count = 0;        // タリフ縦増分数
+                decimal horizontalValue_adding_count = 0; // タリフ横増分数
+                decimal vh_adding_price = 0;              // 両軸の加算額
 
-                // TODO: urgent1 akema 加算ありの範囲の時、両端加算ありの時、
-                // TODO: high akema 繰返し範囲の時
-                var adding_charge = (decimal)0;
+                // TODO: urgent1 akema 加算ありの範囲の時、両端加算ありの時
 
-                return base_charge_before_adding + adding_charge;
+                //  縦軸の加算額 = タリフ縦増分数 * 縦軸加算額
+                if (td.vertical_adding_flg == 1)
+                {
+                    // 縦軸データ取得
+                    var verticalQuery = tariffDs.m_tariff_detail.Where(r => ((r.vertical_step_from == td.vertical_step_from && r.horizontal_step_to >= td.horizontal_step_from)));
+                    var vd = verticalQuery.Select(tdr => tdr).ToArray()[0];
+                    // タリフ縦増分数 = （縦軸値-タリフ縦軸目盛値FR）/ （タリフ縦増分値）
+                    vertical_adding_count = ((vertialValue - vd.vertical_step_from) / vd.vertical_adding_step_value);
+                    vertical_adding_price = vertical_adding_count * vd.vertical_adding_unit_price;
+                }
+
+                // 横軸の加算額 = タリフ横増分数 * 横軸加算額
+                if (td.horizontal_adding_flg == 1)
+                {
+                    // 横軸データ取得
+                    var horizontalQuery = tariffDs.m_tariff_detail.Where(r => ((r.vertical_step_to == td.vertical_step_from && r.horizontal_step_from >= td.horizontal_step_from)));
+                    var hd = horizontalQuery.Select(tdr => tdr).ToArray()[0];
+                    // タリフ横増分数 = （横軸値-タリフ横軸目盛値FR）/（タリフ横増分値）
+                    horizontalValue_adding_count = ((horizontalValue - hd.horizontal_step_from) / hd.horizontal_adding_step_value);
+                    horizontal_adding_price = horizontalValue_adding_count * hd.horizontal_adding_unit_price;
+                }
+
+                // 両軸の加算額
+                if (td.vertical_adding_flg == 1 && td.horizontal_adding_flg == 1)
+                {
+                    // 増分値取得（verticalとhorizontal同値の想定だが、異なった場合は大きい方を取得）
+                    decimal adding_unit_price = td.vertical_adding_unit_price > td.horizontal_adding_unit_price ? td.vertical_adding_unit_price : td.horizontal_adding_unit_price;
+
+                    // 両軸の加算額 = 両軸増分単価 * (タリフ縦増分数 * タリフ横増分数)
+                    vh_adding_price = adding_unit_price * (vertical_adding_count * horizontalValue_adding_count );
+                }
+
+                // 縦軸・横軸・両軸の加算額合計
+                decimal adding_charge = vertical_adding_price + horizontal_adding_price + vh_adding_price;
+                
+                // 可算前 + 加算額合計
+                decimal total_adding_charge = base_charge_before_adding + adding_charge;
+
+                return total_adding_charge;
             }
             else
             {
